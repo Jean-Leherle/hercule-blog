f7262464460d9d28438478a4a6e31108d2aea4d0
Jean-Leherle <j.leherle@gmail.com>
2022-05-27 07:47:29 +0200

    etape 4 : le timide titre vous indique l'evidence donc on le met en petit en haut

--- js/exo.js
+++ js/exo.js
@@ -1,7 +1,7 @@
 console.log('test 1...2...1...2');
 const app = {
     hercule: {
-        name: 'hercule',
+        name: 'Hercule',
         job: 'Demi-dieu',
         age: 35,
         department: 75,
@@ -9,12 +9,19 @@ const app = {
         inRelationship: true,
     },//fin de hercule
     friendList: ['Jupiter', 'Junon', 'Alcmène','Déjanire'],
+    createTitle: function(){
+        const titleElement=document.createElement('h1');
+        titleElement.className='banner__title';
+        titleElement.innerText=`Vous consultez le profil de ${app.hercule.name}`;
+        document.getElementById('header-banner').appendChild(titleElement);
+    },
 
 
     init: function () {
         base.fillProfil(app.hercule);
         base.printFriends(app.friendList);
         base.setBestFriend(app.friendList[0]);
+        app.createTitle();
 
     }//fin de init
 
