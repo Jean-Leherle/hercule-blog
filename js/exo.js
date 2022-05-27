269cb2a7206a9be61b1ea3a8bad5cab1f237e996
Jean-Leherle <j.leherle@gmail.com>
2022-05-27 07:34:06 +0200

    etape 2 : profil maj

--- js/exo.js
+++ js/exo.js
@@ -1,1 +1,20 @@
 console.log('test 1...2...1...2');
+const app = {
+    hercule: {
+        name: 'hercule',
+        job: 'Demi-dieu',
+        age: 35,
+        department: 75,
+        arm: 60.5,
+        inRelationship: true,
+    },
+
+
+    init: function () {
+        base.fillProfil(app.hercule);
+    }//fin de init
+
+} //fin de app
+
+
+document.addEventListener('DOMContentLoaded', app.init);
