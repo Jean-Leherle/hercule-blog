5045b2a409b3a9fe806507f99d110535da9fbe68
Jean-Leherle <j.leherle@gmail.com>
2022-05-27 07:39:44 +0200

    etape 3 : Hercule s'est fait des amis !

--- js/exo.js
+++ js/exo.js
@@ -7,11 +7,15 @@ const app = {
         department: 75,
         arm: 60.5,
         inRelationship: true,
-    },
+    },//fin de hercule
+    friendList: ['Jupiter', 'Junon', 'Alcmène','Déjanire'],
 
 
     init: function () {
         base.fillProfil(app.hercule);
+        base.printFriends(app.friendList);
+        base.setBestFriend(app.friendList[0]);
+
     }//fin de init
 
 } //fin de app
